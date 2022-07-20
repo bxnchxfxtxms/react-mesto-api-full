4978532beac44f44a65bebc6d4ceaab4bf89a652
Dmitriy Shumeikin <bunchomuncho@gmail.com>
2022-07-20 16:19:38 +0300

    ПР-15 проверка сервера

--- backend/app.js
+++ backend/app.js
@@ -33,7 +33,7 @@ app.use((req, res, next) => {
   const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Credentials', true);
+    res.header('Access-Control-Allow-Credentials', true);
   }
   const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
