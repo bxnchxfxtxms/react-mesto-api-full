b7ef11971e26a0b726c365634337f04e58e232d3
Dmitriy Shumeikin <bunchomuncho@gmail.com>
2022-07-22 16:39:35 +0300

    ПР-15 для второй проверки

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const express = require('express');
-// const helmet = require('helmet');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
@@ -23,7 +22,6 @@ const {
 
 const { PORT = 3000 } = process.env;
 const app = express();
-// app.use(helmet());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -92,17 +90,5 @@ app.use(errorLogger);
 app.use(errors());
 
 app.use(errorHandler);
-
-// app.use((err, req, res, next) => {
-//   const { statusCode = 500, message } = err;
-//   res
-//     .status(statusCode)
-//     .send({
-//       message: statusCode === 500
-//         ? 'На сервере произошла ошибка'
-//         : message,
-//     });
-//   next();
-// });
 
 app.listen(PORT);
--- backend/middlewares/errorHandler.js
+++ backend/middlewares/errorHandler.js
@@ -1,15 +1,3 @@
-// app.use((err, req, res, next) => {
-//   const { statusCode = 500, message } = err;
-//   res
-//     .status(statusCode)
-//     .send({
-//       message: statusCode === 500
-//         ? 'На сервере произошла ошибка'
-//         : message,
-//     });
-//   next();
-// });
-
 module.exports = (err, req, res, next) => {
   const { statusCode = 500, message } = err;
   res
@@ -17,7 +5,7 @@ module.exports = (err, req, res, next) => {
     .send({
       message: statusCode === 500
         ? 'На сервере произошла ошибка'
-        : 'ебать тебя в сраку',
+        : message,
     });
   next();
 };
